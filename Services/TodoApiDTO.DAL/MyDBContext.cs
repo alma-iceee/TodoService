1ad17dd002ea30bf46764469eaa753a852484a25
alma-iceee <alma.iceee@gmail.com>
2022-05-25 00:07:17 +0600

    Add DAL

---
+++ Services/TodoApiDTO.DAL/MyDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApiDTO.DAL
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
