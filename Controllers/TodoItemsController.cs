be238f1f2be4b2d1a66ea294907addbb1d58c0dd
alma-iceee <alma.iceee@gmail.com>
2022-05-24 23:18:35 +0600

    Add NLog logger

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,16 +12,22 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
+        private readonly ILogger<TodoItemsController> _logger;
         private readonly TodoContext _context;
 
-        public TodoItemsController(TodoContext context)
+        public TodoItemsController(TodoContext context, ILogger<TodoItemsController> logger)
         {
             _context = context;
+
+            _logger = logger;
+            _logger.LogDebug(1, "NLog injected into TodoItemsController");
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
         {
+            _logger.LogInformation("GetTodoItems API was used");
+
             return await _context.TodoItems
                 .Select(x => ItemToDTO(x))
                 .ToListAsync();
@@ -29,6 +36,8 @@ namespace TodoApi.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
         {
+            _logger.LogInformation("GetTodoItem by id API was used");
+
             var todoItem = await _context.TodoItems.FindAsync(id);
 
             if (todoItem == null)
@@ -42,6 +51,8 @@ namespace TodoApi.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateTodoItem(long id, TodoItemDTO todoItemDTO)
         {
+            _logger.LogInformation("UpdateTodoItem API was used");
+
             if (id != todoItemDTO.Id)
             {
                 return BadRequest();
@@ -71,6 +82,8 @@ namespace TodoApi.Controllers
         [HttpPost]
         public async Task<ActionResult<TodoItemDTO>> CreateTodoItem(TodoItemDTO todoItemDTO)
         {
+            _logger.LogInformation("CreateTodoItem API was used");
+
             var todoItem = new TodoItem
             {
                 IsComplete = todoItemDTO.IsComplete,
@@ -89,6 +102,8 @@ namespace TodoApi.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteTodoItem(long id)
         {
+            _logger.LogInformation("DeleteTodoItem by id API was used");
+
             var todoItem = await _context.TodoItems.FindAsync(id);
 
             if (todoItem == null)
